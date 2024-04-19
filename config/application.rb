4d8f09b057b7d2681895f881387a37914283c58a
Vikash Soni <78948052+V-vk-404@users.noreply.github.com>
2024-04-19 10:08:45 +0530

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Noteapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.hosts << "dev-vksoni.cloud"
+    Rails.application.config.hosts << "dev-vksoni.cloud"
+
+    
 
     # Configuration for the application, engines, and railties goes here.
     #
